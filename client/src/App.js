ef14022a3285e9239134b83ed0f7c0fc13a9ad1f
MatyasBlaha <matyasblaha1@gmail.com>
2024-07-13 15:32:38 +0200

    client side structure prepare

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Link } from "react-router-dom";
 
-import RegistrationForm from "./RegistrationForm";
-import Login from "./Login";
-
-import VerifyEmail from "../src/Components/VerifyEmail";
 
 function App() {
   return (
@@ -15,9 +11,7 @@ function App() {
             <Link to="/login">Login</Link>
         </div>
             <Routes>
-                <Route exact path="/form" element={<RegistrationForm/>} />
-                <Route exact path="/login" element={<Login/>} />
-                <Route exact path="/verify/email/:token" element={<VerifyEmail/>} />
+
             </Routes>
     </div>
   );
