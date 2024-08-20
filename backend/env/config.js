7fe28b2e1fefc21e1646eb89c8a0d1fa9b24bd96
MatyasBlaha <matyasblaha1@gmail.com>
2024-08-20 21:18:44 +0200

    env test

---
+++ backend/env/config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import logger from '../src/apps/shared/log/logger.js';
+
+const loadEnvFile = (envFileName) => {
+    const moduleName = fileURLToPath(import.meta.url);
+    const moduleDir = dirname(moduleName);
+    const envFilePath = `${moduleDir}/${envFileName}`;
+
+    if (fs.existsSync(envFilePath)) {
+        dotenv.config({ path: envFilePath });
+    } else {
+        logger.error(`Environment file ${envFileName} does not exist`);
+        process.exit(1);
+    }
+};
+
+export default loadEnvFile;
